daba24d1824d0abdf499797faaafe8244b0f9640
Ahmad Yousuf <ahmadyousuf2603@gmail.com>
2025-11-12 10:47:22 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,11 @@ setup(
     install_requires=requirements,
     python_requires=">=3.8",
     url="https://github.com/QudsLab/dtor",
-    license="MIT",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
