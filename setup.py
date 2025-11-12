3e39922a24f13cad29e7b59be404384abf6c837d
Ahmad Yousuf <ahmadyousuf2603@gmail.com>
2025-11-12 11:00:03 +0600

    0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="dtor",
-    version="0.0.3",
+    version="0.0.4",
     description="A Tor process management library",
     author="Ahmad Yousuf",
     author_email="0xAhmadYousuf@protonmail.com",
